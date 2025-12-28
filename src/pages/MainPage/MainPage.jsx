4606d58f763a4b835bd946aa304972184cceeccd
Roma Galyh <romagalyh@gmail.com>
2025-12-28 14:20:06 +0200

    fix

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,30 +1,20 @@
 import { NavLink } from 'react-router-dom';
 import style from './MainPage.module.css';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Loader from '../../components/Loader/Loader';
 
 export default function MainPage() {
-  const [loading, setLloading] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLloading(false);
-    }, 1000);
-
-    return () => clearTimeout(timer);
-  }, []);
+  const [loading, setLoading] = useState(true);
 
   return (
     <div className={style.container}>
-      {loading ? (
-        <Loader color="#fff" />
-      ) : (
-        <NavLink to={'/home'} className={style.btn}>
-          <svg className={style.logoSvg}>
-            <use href="/sprite.svg#icon-logo-main" />
-          </svg>
-        </NavLink>
-      )}
+      {loading && <Loader color="#fff" />}
+
+      <NavLink to="/home" className={style.btn}>
+        <svg className={style.logoSvg} onLoad={() => setLoading(false)}>
+          <use href="/sprite.svg#icon-logo-main" />
+        </svg>
+      </NavLink>
     </div>
   );
 }
